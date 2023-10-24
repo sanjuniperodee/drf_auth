47f0fd68080355c5907d4ed68871b7f4ec78e457
sanjuniperodee <87779571856b@gmail.com>
2023-10-24 19:12:37 +0600

    new

--- auth/users/admin.py
+++ auth/users/admin.py
@@ -19,8 +19,22 @@ class PostAdmin(admin.ModelAdmin):
 class PostImageAdmin(admin.ModelAdmin):
     pass
 
+@admin.register(Certificate)
+class CertificateAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'restaurant', 'status',)
+    # list_display = ('id', 'user', 'restaurant', 'status',)
+    # actions = ['mark_as_activated', 'mark_as_non_activated']
+    #
+    # def mark_as_activated(self, request, queryset):
+    #     queryset.update(status=True)
+    #
+    # def mark_as_non_activated(self, request, queryset):
+    #     queryset.update(status=False)
+    #
+    # mark_as_activated.short_description = "Mark selected certificates as Activated"
+    # mark_as_non_activated.short_description = "Mark selected certificates as Non-Activated"
 
-admin.site.register(Certificate)
+# admin.site.register(Certificate)
 admin.site.register(Favorites)
 admin.site.register(Tag)
 admin.site.register(User)
--- auth/users/models.py
+++ auth/users/models.py
@@ -9,6 +9,10 @@ class User(AbstractUser):
     phone_number = models.CharField(max_length=255, unique=True)
     username = models.CharField(max_length=255, unique=True, null=True)
     password = models.CharField(max_length=255, null=True)
+
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
     # USERNAME_FIELD = phone_number
 
     def __str__(self):
@@ -17,8 +21,11 @@ class User(AbstractUser):
 
 
 class Tag(models.Model):
-    title = models.CharField(max_length=255)
+    title = models.CharField(max_length=255, verbose_name='Название')
 
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
     def __str__(self):
         return self.title
 
@@ -31,24 +38,28 @@ class ImageModel(models.Model):
 
 
 class Restaurant(models.Model):
-    logo = models.ImageField(null=True)
-    title = models.CharField(max_length=255)
-    description = models.TextField(null=True)
-    image = models.ImageField(null=True)
-    tags = models.ManyToManyField(Tag, default=None)
-    average = models.CharField(max_length=255, null=True)
-    food_type = models.CharField(max_length=255)
-    phone_number = models.CharField(max_length=255)
-    menu = models.ManyToManyField(ImageModel, default=None, null=True)
-    sales = models.ImageField(null=True, default=None)
-    prices = models.CharField(null=True, max_length=255)
-    slug = models.SlugField(null=True)
-    location = models.CharField(max_length=255, null=True)
-    kitchen = models.CharField(max_length=255, null=True)
-    novy = models.BooleanField(default=False)
-    insta = models.CharField(max_length=255, null=True)
-    whatsapp = models.CharField(max_length=255, null=True)
-    work_hours = models.CharField(max_length=255, null=True)
+    logo = models.ImageField(null=True, verbose_name='Логотип')
+    title = models.CharField(max_length=255, verbose_name='Название')
+    description = models.TextField(null=True, verbose_name='Описание')
+    image = models.ImageField(null=True, verbose_name='Главная фотография')
+    tags = models.ManyToManyField(Tag, default=None, verbose_name='Категории')
+    average = models.CharField(max_length=255, null=True, verbose_name='Средний чек')
+    food_type = models.CharField(max_length=255, verbose_name='Тип еды')
+    phone_number = models.CharField(max_length=255, verbose_name='Номер телефона')
+    menu = models.ManyToManyField(ImageModel, default=None, null=True, verbose_name='Меню')
+    sales = models.ImageField(null=True, default=None, verbose_name='Акции')
+    prices = models.CharField(null=True, max_length=255, verbose_name='Цены для сертификата')
+    slug = models.SlugField(null=True, verbose_name='ссылка')
+    location = models.CharField(max_length=255, null=True, verbose_name='Адресс')
+    kitchen = models.CharField(max_length=255, null=True, verbose_name='Кухня')
+    novy = models.BooleanField(default=False, verbose_name='Скрыть')
+    insta = models.CharField(max_length=255, null=True, verbose_name='Instagram')
+    whatsapp = models.CharField(max_length=255, null=True, verbose_name='Whatsapp')
+    work_hours = models.CharField(max_length=255, null=True, verbose_name='Часы работы')
+
+    class Meta:
+        verbose_name = 'Ресторна'
+        verbose_name_plural = 'Рестораны'
     def __str__(self):
         return self.title + ": " + str(self.pk)
 
@@ -66,18 +77,25 @@ class Favorites(models.Model):
 
 
 class Certificate(models.Model):
-    sum = models.FloatField()
-    user = models.ForeignKey(User, default=None, on_delete=models.CASCADE)
-    encode = models.CharField(max_length=255, null=True, blank=True)
-    status = models.BooleanField(default=False, blank=True)
-    start_date = models.DateTimeField(null=True, blank=True)
-    end_date = models.DateTimeField(null=True, blank=True)
-    restaurant = models.ForeignKey(Restaurant, default=None, on_delete=models.CASCADE, null=True)
+    sum = models.FloatField(verbose_name='Логотип')
+    user = models.ForeignKey(User, default=None, on_delete=models.CASCADE, verbose_name='Пользователь')
+    encode = models.CharField(max_length=255, null=True, blank=True, verbose_name='Код сертификата')
+    status = models.BooleanField(default=False, blank=True, verbose_name='Акитивирован')
+    start_date = models.DateTimeField(null=True, blank=True, verbose_name='Начало активации')
+    end_date = models.DateTimeField(null=True, blank=True, verbose_name='Срок')
+    restaurant = models.ForeignKey(Restaurant, default=None, on_delete=models.CASCADE, null=True, verbose_name='Ресторан')
+
+    class Meta:
+        verbose_name = 'Сертификат'
+        verbose_name_plural = 'Сертификаты'
 
 
 class Status(models.Model):
-    title = models.CharField(max_length=255)
-    body = models.TextField()
+    title = models.CharField(max_length=255, verbose_name='Статус')
+    body = models.TextField(verbose_name='Тело запроса')
 
+    class Meta:
+        verbose_name = 'Статус'
+        verbose_name_plural = 'Статусы'
     def __str__(self):
         return self.title
